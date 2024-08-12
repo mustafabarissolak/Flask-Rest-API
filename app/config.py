cd713e9f4fde137c47c794f603bfde7bafc65632
Barış Solak <mustafabarissolak@gmail.com>
2024-08-12 09:32:05 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 class PostgresqlConfig:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:1234@localhost/swagger"
+    SQLALCHEMY_DATABASE_URI = "postgresql://user_name:password@localhost/db_name"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
@@ -7,8 +7,8 @@ from pymongo import MongoClient
 
 mongo_url = "mongodb://localhost:27017/"
 client = MongoClient(mongo_url)
-mdb = client["swaggerLogs"] 
-
+mdb = client["db_name"] 
+# collection name
 user_logs = mdb["user_logs"]
 crud_logs = mdb["crud_logs"]
 error_logs = mdb["error_logs"]
